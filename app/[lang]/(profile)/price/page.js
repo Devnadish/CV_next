7da88f82ab5cbf9540dc418eaff726d282d25e29
devnadish <devnadish@gmail.com>
2023-09-18 19:40:35 +0300

    update endpoint3

--- app/[lang]/(profile)/price/page.js
+++ app/[lang]/(profile)/price/page.js
@@ -3,16 +3,45 @@ import { Package } from "@/components/pricecompnent/Package";
 import PageContainer from "@/components/shared/pagecontainer/PageContainer";
 import { getDictionary } from "@/lib/dictionary";
 
+
+const getCounter = async () => {
+  try {
+    // const response = await fetch(`http://localhost:3000/api/blog/blogvisitor`,{
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/blog/blogvisitor`,{
+      headers : {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+       }
+    } ,{
+      cache: 'no-cache',
+    });
+    const data = await response.json();
+    // const data = await response.json();
+console.log(data.blogs[0])
+    return data.blogs[0]
+  } catch (error) {
+    console.error('An error occurred during the fetch request:', error);
+    // Handle the error gracefully (e.g., show a fallback value or provide a default counter)
+    return null;
+  }
+};
+
+
+
+
+
 async function page({ params: { lang } }) {
   const {
     page: { pricePkg  },
   } = await getDictionary(lang);
+const dd= await getCounter()
 
-  // console.log(pricePkg);
+
   return (
     <>
       <PageContainer>
       <h1>{process.env.NEXT_PUBLIC_API_URL}</h1>
+        <h1 className="text-2xl text-black">{ dd.user }</h1>
         <div className="grid grid-cols-1 gap-6 lg:grid-cols-3 place-items-start ">
           <Package
             category={pricePkg.standerd.title}
