35adbca154402fdceea7f36fa0fb33f40fa259d6
devnadish <devnadish@gmail.com>
2023-09-20 02:54:37 +0300

    i wish be fixed4

--- components/blogcomponent/ViewerClick.jsx
+++ components/blogcomponent/ViewerClick.jsx
@@ -1,26 +1,40 @@
 "use client";
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import { useRouter } from 'next/navigation'
-function ViewerClick({ title, slug, lang }) {
+function ViewerClick({ title, slug, lang, children }) {
+  const [mounted, setMounted] = useState(false);
   const router = useRouter();
 
   const handleIncrease = async () => {
     try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/api/blog/blogvisitor/${slug}`,
-        {
-          method: "PUT",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(slug),
-        }
-      );
+      const url = `${process.env.NEXT_PUBLIC_API_URL}/api/blog/blogvisitor/${slug}`;
+      const options = {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(slug),
+      };
+
+      const response = await fetch(url, options);
       const data = await response.json();
+
       router.push(`/${lang}/blog/${slug}`);
     } catch (error) {
       console.error(error);
     }
   };
-  return (
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return mounted && <BtnClick handleIncrease={handleIncrease} title={title}/>;
+}
+
+export default ViewerClick
+
+
+const BtnClick=({handleIncrease,title})=>{
+  return(
     <div className="flex items-center justify-center p-2 transition-all transform bg-blue-600/70 dark:bg-zinc-800/90 hover:scale-110">
       <button
         className="font-semibold text-center text-white text-md hover:underline font-tajawal"
@@ -29,7 +43,4 @@ function ViewerClick({ title, slug, lang }) {
         {title}
       </button>
     </div>
-  );
-}
-
-export default ViewerClick
+  )}
