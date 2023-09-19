7fce7826a2403b1b92129c4fc461f7d2e84f1732
devnadish <devnadish@gmail.com>
2023-09-20 01:30:38 +0300

    i wish be fixed2

--- components/blogcomponent/ViewerClick.jsx
+++ components/blogcomponent/ViewerClick.jsx
@@ -1,24 +1,31 @@
 "use client";
 import React from 'react'
 import { useRouter } from 'next/navigation'
-function ViewerClick({title, slug, lang}) {
-    const router = useRouter();
-    const handelIncrese = () => {
-      console.log("ViewerClick")
-        fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/blog/blogvisitor/${slug}`, {
-            method: "PUT",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(slug),
-        }).then((res) => res.json())
-        router.push(`/${lang}/blog/${slug}`)
+function ViewerClick({ title, slug, lang }) {
+  const router = useRouter();
+
+  const handleIncrease = async () => {
+    try {
+      console.log("ViewerClick");
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/blog/blogvisitor/${slug}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(slug),
+        }
+      );
+      const data = await response.json();
+      router.push(`/${lang}/blog/${slug}`);
+    } catch (error) {
+      console.error(error);
     }
-
-
+  };
   return (
-    <div className="flex items-center justify-center p-2 transition-all transform bg-blue-600/70 dark:bg-zinc-800/90 hover:scale-110 ">
+    <div className="flex items-center justify-center p-2 transition-all transform bg-blue-600/70 dark:bg-zinc-800/90 hover:scale-110">
       <button
         className="font-semibold text-center text-white text-md hover:underline font-tajawal"
-        onClick={handelIncrese}
+        onClick={handleIncrease}
       >
         {title}
       </button>
