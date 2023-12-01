79c0eee2859fdd2dffb4475b4631c037638f613b
devnadish <devnadish@gmail.com>
2023-12-02 01:12:42 +0300

    V31

--- components/header/Navbar.jsx
+++ components/header/Navbar.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import Logo from '@/components/header/Logo';
+// import Logo from '@/components/header/Logo';
 import dynamic from 'next/dynamic';
 
 import { getDictionary } from '@/lib/dictionary';
